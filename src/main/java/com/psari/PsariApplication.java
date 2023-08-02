d7b2f0a252fe5d5c32bcb4bc1a830a265a672521
Parksuk2 <parks3960@gmail.com>
2023-08-02 22:13:53 +0900

    Hi

---
+++ src/main/java/com/psari/PsariApplication.java
@@ -0,0 +1,13 @@
+package com.psari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psari/PsariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
